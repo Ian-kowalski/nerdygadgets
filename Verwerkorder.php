ffbaa3b9680605e6c4ffb0b7d11bbbc999a98941
sven <Sven.Reitsma@gmail.com>
2022-11-30 16:10:36 +0100

    Sven 30-11-2022

--- Verwerkorder.php
+++ Verwerkorder.php
@@ -1,10 +1,10 @@
 <?php
 $nawgegevens= array(
     "land" => $_GET[ "country"],
-    "Postcode"=> $_GET["postalcode"],
-    "Huisnummer & toevoeging"=> "Jansen",
-    "plaats"=> "Kerkstraat 1",
-    "straatnaam"=> "1234 AB",
+    "Postcode"=> $_GET[ "postalcode"],
+    "Huisnummer & toevoeging"=> $_GET [ "huisnr"],
+    "plaats"=> $_GET[ "city"],
+    "straatnaam"=> $_GET [ "street"],
 );
 
 print_r($nawgegevens);
