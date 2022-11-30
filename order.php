d19d6cccc0817f09bbdef161e5344856e5a74d8d
Lil <leroymaximvlug@gmail.com>
2022-11-30 15:14:16 +0100

    Verzendkosten naar €0 voor order.php

--- order.php
+++ order.php
@@ -25,7 +25,14 @@ if($cart!=null){
                 <h3>Adres</h3>
                 <label for="postalcode">Postcode</label>
                 <input type="text" name="postalcode" id="postalcode"><br>
-
+                <label for="huisnmr">Huisnmr & toevoeging</label>
+                <input type="text" name="huisnmr" id="huisnmr"><br>
+                <label for="city">Plaats</label>
+                <input type="text" name="city" id="city"><br>
+                <label for="street">Straatnaam</label>
+                <input type="text" name="street" id="street"><br>
+                <label for="city">Plaats</label>
+                <input type="text" name="city" id="city"><br>
             </form>
         </div>
         <div class="totalPrice">
@@ -40,7 +47,7 @@ if($cart!=null){
                 $verzendkosten = 0;
             }
             print ("Subtotaal: €".number_format($totaalPrice, 2, ",", ".")."<br>");
-            print("Verzendkosten: €".number_format($totaalPrice,2,",", "."). "<br>");
+            print("Verzendkosten: €".number_format($verzendkosten,2,",", "."). "<br>");
             print("Totaal: €".number_format($totaalPrice,2,",", "."). "<br>"); ?>
             <a href="/nerdygadgets/order.php" class='button button1'>bestellen</a>
         </div>
