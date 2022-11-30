d6be5d30e5e1bcfb1ce63099a5a8e0345ca4b47b
Lil <leroymaximvlug@gmail.com>
2022-11-30 10:37:37 +0100

    Bestelknop

--- Cart.php
+++ Cart.php
@@ -104,7 +104,7 @@ if($cart!=null){
                     print ("Subtotaal: €".number_format($totaalPrice, 2, ",", ".")."<br>");
                     print("Verzendkosten: €".number_format($totaalPrice,2,",", "."). "<br>");
                     print("Totaal: €".number_format($totaalPrice,2,",", "."). "<br>"); ?>
-
+                    <a href="/nerdygadgets/order.php" class='button button1'>bestellen</a>
                 </div>
         </div>
     <?php
