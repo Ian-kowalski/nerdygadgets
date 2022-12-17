9a3c6fb41008323df4e9670a5ca71a16655828eb
Lil <leroymaximvlug@gmail.com>
2022-12-17 19:57:54 +0100

    Simpele settings page met voor nu alleen een lightmode knop

---
+++ settings.php
@@ -0,0 +1,15 @@
+<?php
+include __DIR__ . "/header.php";
+?>
+
+<h3>Lightmode </h3>
+<!-- Rounded switch -->
+<label class="switch">
+    <input type="checkbox">
+    <span class="slider round"></span>
+</label>
+
+<form>
+    <button type="button">Opslaan?</button>
+</form>
+
