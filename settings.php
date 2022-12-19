eefa4d0755be80f45c23ff840ec56af271c87222
leroy <leroymaximvlug@gmail.com>
2022-12-19 19:46:47 +0100

    Deels werkende slider bij settings

--- settings.php
+++ settings.php
@@ -1,15 +1,26 @@
 <?php
 include __DIR__ . "/header.php";
+include 'darkmode.php';
 ?>
 
 <h3>Lightmode </h3>
 <!-- Rounded switch -->
-<label class="switch">
-    <input type="checkbox">
-    <span class="slider round"></span>
-</label>
 
-<form>
-    <button type="button">Opslaan?</button>
+
+<form method="post">
+    <label class="switch">
+        <input type="checkbox" name="colorswitch" value="colormode" <?php if(isset($_POST['colorswitch'])) echo "checked='checked'"; ?>  />
+        <span class="slider round"></span>
+    </label><br>
+    <div class="button button1">
+        <input type="submit" name="settingsubmit" value="Opslaan">
+    </div>
 </form>
 
+<?php
+
+if(filter_has_var(INPUT_POST, 'colorswitch')) {
+    echo("De slider werkt.");
+}
+
+?>
