d099cbbdfe16087422c7d72e667b35ca6055c486
sven <Sven.Reitsma@gmail.com>
2022-12-21 12:41:53 +0100

    Sven 21-12-2022

--- browse.php
+++ browse.php
@@ -1,7 +1,7 @@
 <!-- dit bestand bevat alle code voor het productoverzicht -->
 <?php
 include __DIR__ . "/header.php";
-
+// test of er een categorie is geselecteerd
 $ReturnableResult = null;
 $Sort = "SellPrice";
 $SortName = "price_low_high";
