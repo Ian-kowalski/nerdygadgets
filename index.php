cb8cc68e32c97828f9337fc81583c09062fb8924
helzz10 <s1187978@student.windesheim.nl>
2022-12-22 15:47:17 +0100

    huts

--- index.php
+++ index.php
@@ -6,18 +6,63 @@ include __DIR__ . "/header.php";
     <div class="col-11">
         <div class="TextPrice">
             <a href="view.php?id=93">
-                <div class="DiscountTag"> 15% Discount</div>
-                <div class="TextMain">
-                    "The Gu" red shirt XML tag t-shirt (Black) M
-                </div>
-                <ul id="ul-class-price">
-                    <li class="HomePagePrice"><i>€36.50</i></li>
-                    <li class="HomePageDiscount">NOW: € 30,95</li>
+                <!-- Slideshow container -->
+                <div class="slideshow-container">
 
-                </ul>
-        </div>
-        </a>
-        <div class="HomePageStockItemPicture"></div>
+                    <!-- Full-width images with number and caption text -->
+                    <div class="mySlides fade">
+                        <img src="slide1.png" style="width:100%">
+                        <div class="text">Caption Text</div>
+                    </div>
+
+                    <div class="mySlides fade">
+                        <img src="slide2.png" style="width:100%">
+                        <div class="text">Caption Two</div>
+                    </div>
+
+                    <!-- Next and previous buttons -->
+                    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                </div>
+                <br>
+
+                <!-- The dots/circles -->
+                <div style="text-align:center">
+                    <span class="dot" onclick="currentSlide(1)"></span>
+                    <span class="dot" onclick="currentSlide(2)"></span>
+                </div>
+
+                <script>
+                    let slideIndex = 1;
+                    showSlides(slideIndex);
+
+                    // Next/previous controls
+                    function plusSlides(n) {
+                        showSlides(slideIndex += n);
+                    }
+
+                    // Thumbnail image controls
+                    function currentSlide(n) {
+                        showSlides(slideIndex = n);
+                    }
+
+                    function showSlides(n) {
+                        let i;
+                        let slides = document.getElementsByClassName("mySlides");
+                        let dots = document.getElementsByClassName("dot");
+                        if (n > slides.length) {slideIndex = 1}
+                        if (n < 1) {slideIndex = slides.length}
+                        for (i = 0; i < slides.length; i++) {
+                            slides[i].style.display = "none";
+                        }
+                        for (i = 0; i < dots.length; i++) {
+                            dots[i].className = dots[i].className.replace(" active", "");
+                        }
+                        slides[slideIndex-1].style.display = "block";
+                        dots[slideIndex-1].className += " active";
+                    }
+                </script>
+            </a>
     </div>
 </div>
 <?php
