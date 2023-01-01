9554c2078ba0e8ba024f45b708f24cacb968dc41
Ian <ian.kowalski@hotmail.com>
2023-01-01 23:56:14 +0100

    work now

--- register.php
+++ register.php
@@ -147,7 +147,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
             if ($stmt = mysqli_prepare($link, $sql)) {
                 // Bind variables to the prepared statement as parameters
-                mysqli_stmt_bind_param($stmt, "isi", $param_ID, $param_name, $param_ID);
+                mysqli_stmt_bind_param($stmt, "isi", $CustomerID, $param_name, $CustomerID);
 
                 // Set parameters
                 $param_name = $voornaam . " " . $achternaam;
