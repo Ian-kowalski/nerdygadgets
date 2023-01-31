572e8591efb5cbf4950cd0a1c33578898e9dc8f6
leroy <leroymaximvlug@gmail.com>
2023-01-31 23:21:10 +0100

    Voorbereidend werk voor korting

---
+++ discount.php
@@ -0,0 +1,23 @@
+<?php
+include __DIR__ . '/header.php';
+include "discountFuncties.php";
+?>
+<h3>Je kortingscode voor vandaag</h3>
+
+<?php
+if (!$_GET['personalDiscount']) {
+    echo("Je hebt nog geen code gegenereerd. Klik op de knop hieronder om dat te doen!");
+} else {
+    $codeGen1 = getCode($n);
+    echo $codeGen1;
+    ?>
+    <br>
+    <?php
+    $codeGen2 = substr(sha1(mt_rand()), 17, 10); //To Generate Random Numbers with Letters.
+    echo $codeGen2;
+}
+?>
+
+<form method="get" class="mt-3">
+    <input type="submit" class='button button1' value="genereer code" name="personalDiscount">
+</form>
---
+++ discountFuncties.php
@@ -0,0 +1,14 @@
+<?php
+$n=10;
+function getCode($n) {
+    $characters = '01234567890123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+
+    for ($i = 0; $i < $n; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $randomString .= $characters[$index];
+    }
+
+    return $randomString;
+}
+?>
